4ca3565616ee499fec4d5bedb64f243b33961fdf
Michael Webb <michaelwebb76@gmail.com>
2025-09-20 14:45:08 +1000

    Set up nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    bellroy-nix-foss.url = "github:bellroy/bellroy-nix-foss";
+  };
+
+  outputs =
+    inputs:
+    inputs.bellroy-nix-foss.lib.haskellProject {
+      src = ./.;
+      supportedCompilers = [
+        "ghc96"
+        "ghc98"
+        "ghc910"
+        "ghc912"
+      ];
+      defaultCompiler = "ghc910";
+    };
+}
